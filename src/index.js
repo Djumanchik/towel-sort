4716ae43dc09182daeebed61e785f9002736fa87
Kirill Dzhumanov <Djum83@mail.ru>
2023-02-07 15:45:45 +0300

    init: test problems

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+const matrix = [
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ],
+]
 
+function towelSort(matrix) {
+  //const res = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      let column = i % 2 === 0 ? k : (matrix[i].length - 1 - k);
+      const res = res.push(matrix[i][column]);
+    }
+  }
+  return res
+}
+
+console.log(towelSort(matrix));
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
